067dac525d72ad0040973444b392f4a3e5ad9241
Paresh Kumar Rout Kumar <101355021+Routparesh@users.noreply.github.com>
2022-09-23 14:48:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Welcome our burger resturant!")
+size = input("What size burger you want? M,N or L: ")
+add_mushroom = input("Do you want mushroom? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+
+if size == "M":
+  bill += 5
+elif size == "N":
+  bill += 8
+else:
+  bill += 10
+if add_mushroom == "Y":
+  if size == "L":
+    bill += 2
+  else:
+    bill += 1
+if extra_cheese == "Y":
+  bill += 1
+print(f"Your final bill is: ${bill}")
